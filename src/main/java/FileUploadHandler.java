e7114b55aec697260ad994277e61d7defc206f10
netlab user <netlabuser@cnislab.dlsu.edu.ph>
2016-03-12 11:38:16 +0800

    Update

--- src/main/java/FileUploadHandler.java
+++ src/main/java/FileUploadHandler.java
@@ -102,7 +102,17 @@ import org.json.simple.JSONObject;
 	}
 	}
 
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		processRequest(request,response);
+	}
 	
 	
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		processRequest(request,response);
+	}
 
 }
